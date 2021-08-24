52f2d2e26932f0c92d22f5a9b0f80c775f63a964
Rico John Dato-on <rico@Ricos-MacBook-Pro.local>
2021-08-24 14:02:02 -0400

    Setup NextJS Hello-World Sample

---
+++ hello-world/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  /* React strict mode is a development mode only feature for highlighting potential problems in an application
+  it helps identify unsafe life cycles legacy api usage and a lot more
+  */
+  reactStrictMode: true,
+}
